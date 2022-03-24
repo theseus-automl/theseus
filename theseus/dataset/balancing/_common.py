aa5ab01961a9c2f0ff15d98a46eac628517127b5
SN4KEBYTE <timurcat7@gmail.com>
2022-03-25 00:14:34 +0700

    Init commit

---
+++ theseus/dataset/balancing/_common.py
@@ -0,0 +1,38 @@
+from typing import (
+    Dict,
+    Tuple,
+)
+
+import pandas as pd
+
+_STRATEGIES = {
+    'under': min,
+    'over': max,
+}
+
+
+def prepare(
+    texts: pd.Series,
+    labels: pd.Series,
+    strategy: str,
+) -> Tuple[pd.DataFrame, Dict[int, int], int]:
+    if strategy not in _STRATEGIES:
+        raise ValueError(
+            f'unknown strategy "{strategy}". '
+            f'Consider using one of the following: {", ".join(_STRATEGIES.keys())}',
+        )
+
+    df = pd.DataFrame(
+        {
+            'texts': texts,
+            'labels': labels,
+        },
+    ).reset_index()
+    counts = {label: len(df[df['labels'] == label]) for label in df['labels'].unique()}
+    target_samples = _STRATEGIES[strategy](counts.values())
+
+    return (
+        df,
+        counts,
+        target_samples,
+    )
