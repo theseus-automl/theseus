e2f541be14b18b7accbead7ec09597d46f8b7ae1
SN4KEBYTE <timurcat7@gmail.com>
2022-03-25 00:15:08 +0700

    Seed can be selected randomly

--- theseus/seed.py
+++ theseus/seed.py
@@ -1,12 +1,22 @@
 import random
+from typing import Optional
 
 import numpy as np
 import torch
 
 
 def seed_everything(
-    seed: int,
+    seed: Optional[int],
 ) -> None:
+    if seed is None:
+        max_seed_value = np.iinfo(np.uint32).max
+        min_seed_value = np.iinfo(np.uint32).min
+
+        seed = random.randint(
+            min_seed_value,
+            max_seed_value,
+        )
+
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
