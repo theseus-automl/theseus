58a97efddd4e7f38aae59c771eef28f54e62e64c
SN4KEBYTE <timurcat7@gmail.com>
2022-04-12 03:10:28 +0700

    Added deviation calculation

--- theseus/dataset/balancing/checkers.py
+++ theseus/dataset/balancing/checkers.py
@@ -1,4 +1,10 @@
+from typing import Tuple
+
+import numpy as np
 import pandas as pd
+
+from theseus.dataset._const import _STRATEGIES
+from theseus.dataset.text_dataset import TextDataset
 
 
 def check_balance(
@@ -17,3 +23,19 @@ def check_balance(
         lengths.add(len(df[df['labels'] == label]))
 
     return len(lengths) == 1
+
+
+def get_abs_deviation(
+    dataset: TextDataset,
+) -> Tuple[float, float]:
+    values, counts = np.unique(
+        dataset.labels,
+        return_counts=True,
+    )
+    target_samples_under = _STRATEGIES['under'](counts)
+    target_samples_over = _STRATEGIES['over'](counts)
+
+    return (
+        np.median([np.abs(val - target_samples_under) / target_samples_under for val in values]),
+        np.median([np.abs(val - target_samples_over) / target_samples_over for val in values]),
+    )
