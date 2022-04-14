7dd7619d4218c79610d842b1a8a4ef097871436b
SN4KEBYTE <timurcat7@gmail.com>
2022-04-15 02:38:25 +0700

    Updated models list

--- theseus/dataset/augmentations/random_replacement.py
+++ theseus/dataset/augmentations/random_replacement.py
@@ -1,39 +1,35 @@
 from random import randint
 from types import MappingProxyType
-from typing import NoReturn
 
 from transformers import pipeline
 
 from theseus.lang_code import LanguageCode
 
 RANDOM_REPLACEMENT_MODELS = MappingProxyType({
-    # TODO: use uncased models, you moron
-    LanguageCode.ENGLISH: 'bert-base-cased',
+    LanguageCode.ENGLISH: 'bert-base-uncased',
     LanguageCode.SPANISH: 'PlanTL-GOB-ES/roberta-base-bne',
     LanguageCode.FRENCH: 'camembert-base',
-    LanguageCode.GERMAN: 'bert-base-german-cased',
+    LanguageCode.GERMAN: 'dbmdz/bert-base-german-uncased',
     LanguageCode.CHINESE: 'bert-base-chinese',
     LanguageCode.WU_CHINESE: 'bert-base-chinese',
     LanguageCode.YUE_CHINESE: 'bert-base-chinese',
     LanguageCode.SWEDISH: 'KB/bert-base-swedish-cased',
-    LanguageCode.FINNISH: 'TurkuNLP/bert-base-finnish-cased-v1',
+    LanguageCode.FINNISH: 'TurkuNLP/bert-base-finnish-uncased-v1',
     LanguageCode.JAPANESE: 'cl-tohoku/bert-base-japanese-whole-word-masking',
     LanguageCode.ARABIC: 'aubmindlab/bert-base-arabertv02',
     LanguageCode.PORTUGUESE: 'neuralmind/bert-base-portuguese-cased',
     LanguageCode.ITALIAN: 'indigo-ai/BERTino',
-    LanguageCode.TURKISH: 'dbmdz/electra-base-turkish-mc4-cased-generator',
+    LanguageCode.TURKISH: 'dbmdz/convbert-base-turkish-mc4-uncased',
     LanguageCode.DUTCH: 'GroNLP/bert-base-dutch-cased',
     LanguageCode.UKRAINIAN: 'youscan/ukr-roberta-base',
     LanguageCode.INDONESIAN: 'indolem/indobert-base-uncased',
     LanguageCode.HINDI: 'neuralspace-reverie/indic-transformers-hi-bert',
     LanguageCode.KOREAN: 'klue/bert-base',
     LanguageCode.PERSIAN: 'HooshvareLab/albert-fa-zwnj-base-v2',
-
-    # correct models
     LanguageCode.DANISH: 'Maltehb/danish-bert-botxo',
     LanguageCode.POLISH: 'dkleczek/bert-base-polish-uncased-v1',
-    LanguageCode.ROMANIAN: 'Geotrend/bert-base-ro-cased',  # uncased model is not available
-    LanguageCode.NORWEGIAN: 'NbAiLab/nb-bert-base',  # uncased model is not available
+    LanguageCode.ROMANIAN: 'Geotrend/bert-base-ro-cased',
+    LanguageCode.NORWEGIAN: 'NbAiLab/nb-bert-base',
     LanguageCode.CATALAN: 'PlanTL-GOB-ES/roberta-base-ca',
     LanguageCode.GREEK: 'nlpaueb/bert-base-greek-uncased-v1',
     LanguageCode.VIETNAMESE: 'nguyenvulebinh/envibert',
@@ -62,14 +58,46 @@ RANDOM_REPLACEMENT_MODELS = MappingProxyType({
     LanguageCode.BASQUE: 'ixa-ehu/roberta-eus-euscrawl-base-cased',
     LanguageCode.GUJARATI: 'surajp/RoBERTa-hindi-guj-san',
     LanguageCode.TAGALOG: 'jcblaise/roberta-tagalog-base',
-
+    LanguageCode.SINHALA: 'keshan/SinhalaBERTo',
+    LanguageCode.TELUGU: 'castorini/afriberta_large',
+    LanguageCode.SERBIAN: 'macedonizer/sr-roberta-base',
+    LanguageCode.MACEDONIAN: 'macedonizer/mk-roberta-base',
+    LanguageCode.GALICIAN: 'marcosgg/bert-base-gl-cased',
+    LanguageCode.MALAY: 'malay-huggingface/bert-base-bahasa-cased',
+    LanguageCode.MONGOLIAN: 'hfl/cino-base-v2',
+    LanguageCode.IRISH: 'DCU-NLP/bert-base-irish-cased-v1',
+    LanguageCode.MALAYALAM: 'monsoon-nlp/muril-adapted-local',
+    LanguageCode.AMHARIC: 'castorini/afriberta_large',
+    LanguageCode.PUNJABI: 'monsoon-nlp/muril-adapted-local',
+    LanguageCode.KANNADA: 'monsoon-nlp/muril-adapted-local',
+    LanguageCode.NEPALI: 'monsoon-nlp/muril-adapted-local',
+    LanguageCode.JAVANESE: 'w11wo/javanese-bert-small',
+    LanguageCode.WESTERN_FRISIAN: 'GroNLP/bert-base-dutch-cased-frisian',
+    LanguageCode.GUARANI: 'mmaguero/multilingual-bert-gn-base-cased',
+    LanguageCode.DIVEHI: 'monsoon-nlp/dv-labse',
+    LanguageCode.SOMALI: 'castorini/afriberta_large',
+    LanguageCode.SUNDANESE: 'w11wo/sundanese-roberta-base',
+    LanguageCode.UIGHUR: 'hfl/cino-base-v2',
+    LanguageCode.SANSKRIT: 'surajp/RoBERTa-hindi-guj-san',
+    LanguageCode.KAZAKH: 'hfl/cino-base-v2',
+    LanguageCode.BOSNIAN: 'macedonizer/ba-roberta-base',
+    LanguageCode.TIBETIAN: 'hfl/cino-base-v2',
+    LanguageCode.LAO: 'w11wo/lao-roberta-base',
+    LanguageCode.UZBEK: 'coppercitylabs/uzbert-base-uncased',
+    LanguageCode.LATIN: 'cook/cicero-similis',
+    LanguageCode.BIHARI: 'ibraheemmoosa/xlmindic-base-uniscript',
+    LanguageCode.TAJIK: 'muhtasham/TajBERTo',
+    LanguageCode.SINDHI: 'monsoon-nlp/muril-adapted-local',
+    LanguageCode.QUECHUA: 'Llamacha/QuBERTa',
+    LanguageCode.ALBANIAN: 'macedonizer/al-roberta-base',
+    LanguageCode.TOSK_ALBANIAN: 'macedonizer/al-roberta-base',
 })
 
 
 class RandomReplacementAugmenter:
     def __init__(
         self,
-    ) -> NoReturn:
+    ) -> None:
         self._unmasker = pipeline(
             'fill-mask',
             model='bert-base-multilingual-cased',
@@ -95,9 +123,6 @@ class RandomReplacementAugmenter:
 
         for res in augmentations:
             if res['token_str'] != orig_word:
-                augmented_text = res['sequence'].replace(self._cls_token, '').replace(self._sep_token, '').strip()
-                break
-        else:
-            raise ValueError('unable to generate output different from input')
+                return res['sequence'].replace(self._cls_token, '').replace(self._sep_token, '').strip()
 
-        return augmented_text
+        return text
