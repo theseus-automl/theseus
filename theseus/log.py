13a1e04b09e88db1bf0d304a92fd977377c5c249
SN4KEBYTE <timurcat7@gmail.com>
2022-04-16 00:47:08 +0700

    Added colored logging

--- theseus/log.py
+++ theseus/log.py
@@ -1,49 +1,43 @@
 import logging
 import sys
 
-from theseus.utils import extract_kwargs
-
-# from enum import Enum
+from enum import Enum
 
 
+class _Color(Enum):
+    grey = '\x1b[38;20m'
+    yellow = '\x1b[33;20m'
+    red = '\x1b[31;20m'
+    bold_red = '\x1b[31;1m'
+    reset = '\x1b[0m'
 
-# class _Color(Enum):
-#     grey = '\x1b[38;20m'
-#     yellow = '\x1b[33;20m'
-#     red = '\x1b[31;20m'
-#     bold_red = '\x1b[31;1m'
-#     reset = '\x1b[0m'
-#
-#
-# class ColoredFormatter(logging.Formatter):
-#     def __init__(
-#         self,
-#         fmt: str,
-#         *args,
-#         **kwargs,
-#     ) -> None:
-#         super().__init__(
-#             fmt,
-#             *args,
-#             **kwargs,
-#         )
-#
-#         self._formats = {
-#             logging.DEBUG: _Color.grey.value + fmt + _Color.reset.value,
-#             logging.INFO: _Color.grey.value + fmt + _Color.reset.value,
-#             logging.WARNING: _Color.yellow.value + fmt + _Color.reset.value,
-#             logging.ERROR: _Color.red.value + fmt + _Color.reset.value,
-#             logging.CRITICAL: _Color.bold_red.value + fmt + _Color.reset.value
-#         }
-#
-#     def format(
-#         self,
-#         record: logging.LogRecord,
-#     ) -> str:
-#         log_fmt = self._formats.get(record.levelno)
-#         formatter = logging.Formatter(log_fmt)
-#
-#         return formatter.format(record)
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(
+        self,
+        fmt: str,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            fmt,
+            *args,
+            **kwargs,
+        )
+
+        self._formats = {
+            logging.DEBUG: logging.Formatter(_Color.grey.value + fmt + _Color.reset.value),
+            logging.INFO: logging.Formatter(_Color.grey.value + fmt + _Color.reset.value),
+            logging.WARNING: logging.Formatter(_Color.yellow.value + fmt + _Color.reset.value),
+            logging.ERROR: logging.Formatter(_Color.red.value + fmt + _Color.reset.value),
+            logging.CRITICAL: logging.Formatter(_Color.bold_red.value + fmt + _Color.reset.value),
+        }
+
+    def format(
+        self,
+        record: logging.LogRecord,
+    ) -> str:
+        return self._formats.get(record.levelno).format(record)
 
 
 def setup_logger(
@@ -52,19 +46,16 @@ def setup_logger(
     fmt: str = '%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s',
     **kwargs,
 ) -> logging.Logger:
-    formatter = logging.Formatter(
-        fmt,
-        **extract_kwargs(
-            logging.Formatter.__init__,
-            **kwargs,
-        ),
-    )
-
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
     stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(formatter)
+    stdout_handler.setFormatter(
+        ColoredFormatter(
+            fmt,
+            **kwargs,
+        ),
+    )
     logger.addHandler(stdout_handler)
 
     return logger
