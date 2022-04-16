45560d03ca9342043a8d0a68e8d59a29f60b4154
SN4KEBYTE <timurcat7@gmail.com>
2022-04-16 17:07:51 +0700

    Generated texts are now splitted

--- tests/test_balancing/dataset.py
+++ tests/test_balancing/dataset.py
@@ -7,14 +7,30 @@ from string import ascii_letters
 import pandas as pd
 
 from theseus.dataset.text_dataset import TextDataset
+from theseus.utils import chunkify
 
 _MIN_TEXT_LEN = 5
 _MAX_TEXT_LEN = 20
+_MIN_SPLITS = 4
+_MAX_SPLITS = _MAX_TEXT_LEN
 _DATASET_SIZE = 100
 
 
+def random_split(
+    text: str,
+) -> str:
+    max_chunks = min(
+        len(text) - 1,
+        _MAX_SPLITS,
+    )
+    return ' '.join(''.join(chunk) for chunk in chunkify(list(text), randint(_MIN_SPLITS, max_chunks)))
+
+
 def generate_text() -> str:
-    return ''.join(choice(ascii_letters) for _ in range(randint(_MIN_TEXT_LEN, _MAX_TEXT_LEN)))
+    text = ''.join(choice(ascii_letters) for _ in range(randint(_MIN_TEXT_LEN, _MAX_TEXT_LEN)))
+    text = random_split(text)
+
+    return text
 
 
 def prepare_balanced_dataset() -> TextDataset:
