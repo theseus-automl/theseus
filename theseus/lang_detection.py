c75c8612fde81a98bfc8200c98dc6fb90ceb0a32
SN4KEBYTE <timurcat7@gmail.com>
2022-05-05 02:58:47 +0700

    Moved to new wrapper

--- theseus/lang_detection.py
+++ theseus/lang_detection.py
@@ -1,53 +1,14 @@
 from collections import Counter
 from pathlib import Path
 from typing import (
-    Any,
-    Dict,
     List,
     Union,
 )
 
-from fasttext import (
-    FastText,
-    load_model,
-)
-
 from theseus.exceptions import ModelNotFoundError
+from theseus.wrappers.picklable_fast_text import PicklableFastText
 
 _FT_THRESHOLD = 0
-
-FastText.eprint = lambda _: None
-
-
-class FasttextWrapper:
-    def __init__(
-        self,
-        model_path: Path,
-    ) -> None:
-        self._model_path = model_path
-        self._model = load_model(str(self._model_path))
-
-    def predict(
-        self,
-        *args: Any,
-        **kwargs: Any,
-    ) -> str:
-        return self._model.predict(
-            *args,
-            **kwargs,
-        )
-
-    def __getstate__(
-        self,
-    ) -> Dict[str, Path]:
-        return {'path': self._model_path}
-
-    def __setstate__(
-        self,
-        state: Dict[str, Path],
-    ) -> None:
-        self._model_path = state['path']
-        self._model = load_model(str(self._model_path))
 
 
 class LanguageDetector:
@@ -58,7 +19,7 @@ class LanguageDetector:
         if not model_path.exists() or not model_path.is_file():
             raise ModelNotFoundError('language detection model does not exist or is not a file')
 
-        self._model = FasttextWrapper(model_path)
+        self._model = PicklableFastText(model_path)
 
     def __call__(
         self,
