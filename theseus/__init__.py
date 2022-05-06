a5f28149ee64e6b4b26274b0eb5c967b0eb399f6
SN4KEBYTE <timurcat7@gmail.com>
2022-05-07 01:53:47 +0700

    __all__ for nice imports

---
+++ theseus/__init__.py
@@ -0,0 +1,11 @@
+from theseus.accelerator import Accelerator
+from theseus.classification.auto import AutoClassifier
+from theseus.dataset.text_dataset import TextDataset
+from theseus.zero_shot.auto import AutoZeroShotClassifier
+
+__all__ = [
+    'Accelerator',
+    'AutoClassifier',
+    'AutoZeroShotClassifier',
+    'TextDataset',
+]
