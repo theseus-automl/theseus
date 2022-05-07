0fcb2064bf9380f53abbd1b8da8fef064ecb0066
SN4KEBYTE <timurcat7@gmail.com>
2022-05-07 18:19:35 +0700

    Added default parameters

--- theseus/wrappers/dense_tf_idf.py
+++ theseus/wrappers/dense_tf_idf.py
@@ -3,6 +3,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class DenseTfidfVectorizer(TfidfVectorizer):
+    def __init__(
+        self,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            input='content',
+            encoding='utf-8',
+            decode_error='ignore',
+            binary=False,
+            norm='l2',
+            use_idf=True,
+            smooth_idf=True,
+            sublinear_tf=False,
+            **kwargs,
+        )
+
     def transform(
         self,
         raw_documents,
