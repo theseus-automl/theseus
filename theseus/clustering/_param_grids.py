b6e32bfc6e408a929f53d4764156b7eb4b4f9ead
SN4KEBYTE <timurcat7@gmail.com>
2022-05-28 22:39:03 +0700

    Added dynamic param grid creation

--- theseus/clustering/_param_grids.py
+++ theseus/clustering/_param_grids.py
@@ -1,42 +1,81 @@
+from collections import namedtuple
 from types import MappingProxyType
 
 import numpy as np
+from psutil import cpu_count
 from sklearn.cluster import (
     AffinityPropagation,
     AgglomerativeClustering,
-    Birch,
-    DBSCAN,
     MeanShift,
-    OPTICS,
+    MiniBatchKMeans,
+    KMeans,
 )
 
-CLUSTERERS = (
-    (
-        AffinityPropagation,
-        MappingProxyType({
-            'max_iter': (1000,),
-            'damping': (
-                0.5,
-                0.6,
-                0.7,
-                0.8,
-                0.9,
-            ),
-        }),
-    ),
-    (
+_SizeRange = namedtuple(
+    '_SizeRange',
+    'min max',
+)
+
+# everything else is considered as large
+_SMALL_DATASET_SIZE_RANGE = _SizeRange(
+    min=50,
+    max=9999,
+)
+_MEDIUM_DATASET_SIZE_RANGE = _SizeRange(
+    min=10000,
+    max=99999,
+)
+
+_AFFINITY_PROPAGATION = (
+    AffinityPropagation,
+    MappingProxyType({
+        'max_iter': (1000,),
+        'damping': (
+            0.5,
+            0.6,
+            0.7,
+            0.8,
+            0.9,
+        ),
+    }),
+)
+
+
+def _get_kmeans_num_clusters(
+    dataset_size: int,
+) -> tuple:
+    nums = []
+    exponent = 1
+    result = 0
+
+    while result <= dataset_size:
+        result = 2 ** exponent
+        exponent += 1
+
+        nums.append(result)
+
+    return tuple(nums)
+
+
+def _prepare_mean_shift(
+    dataset_size: int,
+) -> tuple:
+    min_bin_freq = sorted(set(np.ceil(dataset_size * perc) for perc in np.linspace(0.01, 0.1, num=10)))
+
+    return (
         MeanShift,
         MappingProxyType({
             'max_iter': (1000,),
-            'min_bin_freq': (
-                1,
-                5,
-                10,
-            ),
+            'min_bin_freq': tuple(min_bin_freq),
             'n_jobs': (-1,),
         }),
-    ),
-    (
+    )
+
+
+def _prepare_agglomerative(
+    is_small: bool,
+) -> tuple:
+    return (
         AgglomerativeClustering,
         MappingProxyType({
             'affinity': (
@@ -47,68 +86,57 @@ CLUSTERERS = (
                 0.5,
                 0.7,
                 0.9,
-            )
+            ),
+            'linkage': ('ward', 'complete', 'average') if is_small else ('single',),
         }),
-    ),
-    (
-        DBSCAN,
-        MappingProxyType({
-            'eps': (
-                0.1,
-                0.3,
-                0.5,
+    )
+
+
+def make_param_grid(
+    dataset_size: int,
+) -> tuple:
+    if dataset_size < _SMALL_DATASET_SIZE_RANGE.min:
+        raise ValueError('your dataset is too small for clustering algorithms')
+
+    if _SMALL_DATASET_SIZE_RANGE.min <= dataset_size <= _SMALL_DATASET_SIZE_RANGE.max:
+        return (
+            _prepare_mean_shift(dataset_size),
+            _prepare_agglomerative(True),
+            _AFFINITY_PROPAGATION,
+            (
+                KMeans,
+                MappingProxyType({
+                    'n_clusters': _get_kmeans_num_clusters(dataset_size),
+                    'max_iter': (1000,),
+                    'algorithm': (
+                        'lloyd',
+                        'elkan',
+                    ),
+                }),
             ),
-            'min_samples': (
-                5,
-                25,
-                50,
-                75,
-                100,
+        )
+
+    if _MEDIUM_DATASET_SIZE_RANGE.min <= dataset_size <= _MEDIUM_DATASET_SIZE_RANGE.max:
+        return (
+            _prepare_agglomerative(False),
+            _AFFINITY_PROPAGATION,
+            (
+                KMeans,
+                MappingProxyType({
+                    'n_clusters': _get_kmeans_num_clusters(dataset_size),
+                    'max_iter': (1000,),
+                    'algorithm': ('lloyd',),
+                }),
             ),
-            'leaf_size': (
-                5,
-                25,
-                50,
-                100,
-            ),
-            'n_jobs': (-1,),
-        }),
-    ),
-    (
-        OPTICS,
-        MappingProxyType({
-            'min_samples': np.linspace(
-                start=0.1,
-                stop=1,
-                num=10,
-            ),
-            'max_eps': np.linspace(
-                start=0.1,
-                stop=0.5,
-                num=5,
-            ),
-            'metric': (
-                'euclidean',
-                'cosine',
-            ),
-            'leaf_size': (
-                5,
-                25,
-                50,
-                100,
-            ),
-            'n_jobs': (-1,),
-        }),
-    ),
-    (
-        Birch,
-        MappingProxyType({
-            'threshold': np.linspace(
-                start=0.1,
-                stop=1,
-                num=10,
-                endpoint=False,
-            ),
-        }),
-    ),
-)
+        )
+
+    return (
+        (
+            MiniBatchKMeans,
+            MappingProxyType({
+                'n_clusters': _get_kmeans_num_clusters(dataset_size),
+                'max_iter': (1000,),
+                'batch_size': (256 * cpu_count(logical=False),),
+            }),
+        ),
+    )
