157bd3af81ce4027deac8cb432bbc16aa41183c9
SN4KEBYTE <timurcat7@gmail.com>
2022-06-05 01:51:09 +0700

    Added batching and device selection

--- theseus/zero_shot/auto.py
+++ theseus/zero_shot/auto.py
@@ -9,7 +9,10 @@ import pandas as pd
 from theseus.abc.auto_estimator import AutoEstimator
 from theseus.accelerator import Accelerator
 from theseus.dataset.text_dataset import TextDataset
-from theseus.exceptions import UnsupportedLanguageError
+from theseus.exceptions import (
+    DeviceError,
+    UnsupportedLanguageError,
+)
 from theseus.lang_code import LanguageCode
 from theseus.log import setup_logger
 from theseus.plotting.classification import plot_class_distribution
@@ -24,6 +27,7 @@ class AutoZeroShotClassifier(AutoEstimator):
         out_dir: Path,
         accelerator: Accelerator,
         candidate_labels: List[str],
+        batch_size: int = 1,
         target_lang: Optional[LanguageCode] = None,
     ) -> None:
         super().__init__(
@@ -32,6 +36,7 @@ class AutoZeroShotClassifier(AutoEstimator):
             target_lang,
         )
 
+        self._batch_size = batch_size
         self._candidate_labels = candidate_labels
 
     def fit(
@@ -42,9 +47,20 @@ class AutoZeroShotClassifier(AutoEstimator):
         self._detect_lang(dataset.texts)
 
         try:
+            device_num = self._accelerator.select_single_gpu().index
+        except DeviceError as err:
+            _logger.error(f'DeviceError occurred: {err}')
+            _logger.error('unable to select GPU, falling back to CPU...')
+            device_num = -1
+        else:
+            _logger.debug(f'using GPU #{device_num}')
+
+        try:
             clf = ZeroShotClassifier(
                 self._target_lang,
                 self._candidate_labels,
+                device_num=device_num,
+                batch_size=self._batch_size,
             )
         except UnsupportedLanguageError as err:
             _logger.error(str(err))
@@ -62,8 +78,8 @@ class AutoZeroShotClassifier(AutoEstimator):
         out_path: Path,
     ) -> None:
         df = pd.DataFrame()
-        df['texts'] = texts
-        df['labels'] = [model(text) for text in texts]
+        df['text'] = texts
+        df['label'], df['score'] = model(texts)  # noqa: WPS414
 
         df_path = out_path / 'predictions.parquet.gzip'
         _logger.info(f'saving predictions to {df_path}')
@@ -73,6 +89,6 @@ class AutoZeroShotClassifier(AutoEstimator):
         )
 
         plot_class_distribution(
-            df['labels'],
+            df['label'],
             out_path / 'class_distribution.png',
         )
